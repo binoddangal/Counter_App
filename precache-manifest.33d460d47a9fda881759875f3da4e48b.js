a349e09fa9f91e53b263c04058fd6a40249a4f61
binoddangal <dangalbinod555@gmail.com>
2020-04-20 17:38:15 +0545

    Updates

---
+++ precache-manifest.33d460d47a9fda881759875f3da4e48b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78f2c3e603b6607b3eff95c66e1fb2f7",
+    "url": "/Counter_App_React/index.html"
+  },
+  {
+    "revision": "8d146bb6c27c38c38baa",
+    "url": "/Counter_App_React/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "61bdec4216089cbef37a",
+    "url": "/Counter_App_React/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8d146bb6c27c38c38baa",
+    "url": "/Counter_App_React/static/js/2.62ed2149.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Counter_App_React/static/js/2.62ed2149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61bdec4216089cbef37a",
+    "url": "/Counter_App_React/static/js/main.4257d187.chunk.js"
+  },
+  {
+    "revision": "d9419802845fe24d44de",
+    "url": "/Counter_App_React/static/js/runtime-main.0fc81734.js"
+  }
+]);
